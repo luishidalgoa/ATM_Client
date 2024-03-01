86da8bf233d977e7eb89a9e66473602bcf8578c0
Jose Javier Galan <josejaviergalan2003@gmail.com>
2024-03-01 10:11:01 +0100

    inicio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module dev.iesfranciscodelosrios.atm_client {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens dev.iesfranciscodelosrios.atm_client to javafx.fxml;
+    opens dev.iesfranciscodelosrios.atm_client.Controller to javafx.fxml; // Agregado para abrir el paquete que contiene HomeController
+
     exports dev.iesfranciscodelosrios.atm_client;
 }
